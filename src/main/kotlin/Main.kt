0b29495b4c31c41a14d1a0a1ffecb2c4bc6a7276
rahul <meetmeerahul@gmail.com>
2025-10-22 20:50:04 +0530

    when and When (argument)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,13 +4,20 @@ fun main(){
     val formatedInput=input.toIntOrNull()
 
     if(formatedInput!=null){
-        val isEven=formatedInput%2==0;
 
-        val output= if(formatedInput%2==0){
-            "isEven"
-        }
-        else{
-            "is Odd"
+
+      val output=  when(formatedInput){
+//            formatedInput %2==0 && formatedInput>10  -> "Even and greater than 10"
+//            formatedInput %2!=0 && formatedInput <10 && formatedInput>0  -> "Odd and less than 10"
+//            formatedInput %2!=0  && formatedInput>0 -> "odd"
+//            formatedInput %2==0 && formatedInput>0  -> "even"
+//            else -> "Negaive number"
+
+       3->"number is three"
+          5->"number is  five"
+          in 10.. 20 ->"between 10 and 20"
+          else->"I am confussed"
+
         }
 
         println(output)

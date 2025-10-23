dc0d1f275a7c373518e78db20570c48ef95199f8
rahul <meetmeerahul@gmail.com>
2025-10-23 19:26:30 +0530

    Try catch added

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,30 +1,27 @@
 fun main(){
 
  val input=readln()
-    val formatedInput=input.toIntOrNull()
+    val formatedInput=try{
+        input.toInt()
+    } catch(e:NumberFormatException){
+        0
+    }
 
-    if(formatedInput!=null){
+
+
 
 
       val output=  when(formatedInput){
-//            formatedInput %2==0 && formatedInput>10  -> "Even and greater than 10"
-//            formatedInput %2!=0 && formatedInput <10 && formatedInput>0  -> "Odd and less than 10"
-//            formatedInput %2!=0  && formatedInput>0 -> "odd"
-//            formatedInput %2==0 && formatedInput>0  -> "even"
-//            else -> "Negaive number"
 
        3->"number is three"
           5->"number is  five"
           in 10.. 20 ->"between 10 and 20"
-          else->"I am confussed"
+          else->"I am confussed what to print"
 
         }
 
         println(output)
-    }
-    else{
-        println("Null value is not valid")
-    }
+
 
 
 }

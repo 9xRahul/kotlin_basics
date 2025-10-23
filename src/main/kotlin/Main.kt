d7e7f5ce0c7d9172d0e176467f1266ffc0299b39
rahul <meetmeerahul@gmail.com>
2025-10-23 20:44:01 +0530

    Strings

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,14 @@
 fun main(){
- println("Enter the limit")
-  val limit= readln().toIntOrNull()?:0
+ println("Enter the a string")
+  val str= readln();
 
-    var i=0
-    var sum=0
-    var nums=mutableListOf<Int>();
-
-    for(i in 0 until limit){
-        println("Enter ${i+1}th number")
-        val num=readln().toIntOrNull()?:continue
-        nums.add(num);
-    }
-
-
-  for( number in nums){
-      println("$number")
-  }
+    val finalStr= buildString {  for(i in str.lastIndex downTo 0){
+         append(str[i])
+    } }
 
 
 
 
+ print(finalStr)
 
 }

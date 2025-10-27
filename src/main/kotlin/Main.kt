7780001e76a6cff683f54ed9026730bdd3f91de1
rahul <meetmeerahul@gmail.com>
2025-10-27 12:16:35 +0530

    OOPS 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,18 +2,28 @@ import kotlin.math.sqrt
 
 fun main() {
 
-    val rect1=Rectangle(12,12)
+    val rect1=Rectangle(name="first",1288,12)
+    val rect2=Rectangle(name="second",122,126)
 
-    println(rect1.perimeter)
+ val r=  maxPerimeter(rect1, rect2)
+
+    println("${r.name} has perimeter ${r.perimeter}.")
 
 }
 
 
+fun maxPerimeter( rect1:Rectangle,rect2:Rectangle):Rectangle{
 
-class Rectangle(val width: Int, val height: Int) {
+    if(rect1.perimeter<rect2.perimeter)
+        return  rect2
+    else
+        return rect1
+}
+
+class Rectangle(val name:String,  val width: Int, val height: Int) {
 
 
-    val perimeter =4*(width*height)
+    val perimeter =2*(width+height)
 
 
 }

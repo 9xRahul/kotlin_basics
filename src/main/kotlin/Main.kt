d04abb2dfe306ea9bf30e3150aa3ca8ec6105f69
rahul <meetmeerahul@gmail.com>
2025-10-28 19:14:54 +0530

    interfaces

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,22 @@
+import kotlin.math.PI
 import kotlin.math.sqrt
 
 fun main() {
 
-    val rect1=Rectangle(name="first",12,12)
-    val rect2=Rectangle(name="first",12,12)
-
- val r=  maxPerimeter(rect1, rect2)
-
-    println("${r.name} has perimeter ${r.perimeter}.")
+    val rect1=Rectangle(name="first",12f,12f)
+    val rect2=Rectangle(name="first",12f,12f)
 
 
-    print(rect1==rect2)
+    println("Rect 2 arrea : ${rect2.area} ${rect2.circumference}")
+
+    val circle =Circle(15f);
+
+    println("Circle Area : ${circle.area} Circumference : ${circle.circumference}")
+
+
+    println(sumAreas(rect1, circle))
+
+
 
 }
 
@@ -23,10 +29,36 @@ fun maxPerimeter( rect1:Rectangle,rect2:Rectangle):Rectangle{
         return rect1
 }
 
-data class Rectangle(val name:String,  val width: Int, val height: Int) {
+data class Rectangle(val name:String, val width: Float, val height: Float,
+
+):Shape {
+
 
 
     val perimeter =2*(width+height)
 
+    override val area: Float= width * height
 
+    override val circumference: Float=2*(height * width)
+
+}
+
+fun sumAreas(vararg shapes: Shape): Double{
+    return shapes.sumOf { currentShape->
+        currentShape.area.toDouble()
+    }
+}
+
+
+data class Circle(val radius:Float):Shape{
+
+    override val area: Float
+        get() = 2 * PI.toFloat() * radius * radius;
+    override val circumference: Float
+        get() = 2 * PI.toFloat() * radius
+}
+
+interface  Shape{
+    val area:Float
+    val circumference:Float
 }
